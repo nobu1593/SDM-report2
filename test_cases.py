c4b0070db309e8deeeb0d2f70db2206e86d6aa11
nobu1593 <nobuyoshit1593@gmail.com>
2025-02-02 12:17:47 +0900

    テストケースを変更

--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,22 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
+                self.assertEqual (999, calc(1,999))
 
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
+                self.assertEqual (-1, calc(1,0))
 
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
+                self.assertEqual (-1, calc(1,1000))
 
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
+                self.assertEqual (-1, calc(0,999))
 
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(1000,999))
+
+        def test_sample6 (self):
+                self.assertEqual (-1, calc(1.5,999))
+
+        def test_sample7 (self):
+                self.assertEqual (-1, calc('t',999))
